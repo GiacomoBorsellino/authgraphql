383bdb3fe11204266c7eeb05ff5576c153249c6a
Giacomo <giacomo.borsellino@avrgroup.it>
2023-02-13 12:13:07 +0100

    middleware rest

--- BE/src/ServerGraphQl/middleware/middleware.ts
+++ BE/src/ServerGraphQl/middleware/middleware.ts
@@ -11,31 +11,29 @@ const prisma = new PrismaClient()
 const checkToken = async (resolve, root, args, context, info) => {
     console.log('================= IN MIDDLEWARE CHECKTOKEN');
 
-    // log di controllo
-    // console.log('MID 1: ', resolve);
-    // console.log('MID 2: ', root);
-    // console.log('MID 3: ', args);
-    // console.log('MID 4: ', context);
-    // console.log('MID 5: ', info);
-    // console.log('MID 6: ', info.fieldName);
-    // console.log('MID 7: ', context[1].tag);            
-
+    // Log di controllo
     /*
-        Controllo della chiamata attraverso i permessi: 
-        1. Se bisogna fare un controllo del token
-        2. Quali ruoli può fare la chiamata
+        console.log('MID 1: ', resolve);
+        console.log('MID 2: ', root);
+        console.log('MID 3: ', args);
+        console.log('MID 4: ', context);
+        console.log('MID 5: ', info);
+        console.log('MID 6: ', info.fieldName);
+        console.log('MID 7: ', context[1].tag);            
     */
-    let tagResolver = context[1].operationName
+
+    // Controllo della chiamata attraverso i permessi: 
+    // 1. Se bisogna fare un controllo del token
+    // 2. Quali ruoli può fare la chiamata
+    let tagResolver = context[1].operationName;
     let rolesRoute: string[];
-    let checkField: boolean
+    let checkField: boolean;
     permessi.forEach((field) => {
         if (field.operationName === tagResolver) {
             checkField = field.checkToken;
             rolesRoute = field.roles;
         }
     })
-    // console.log(rolesRoute, tagResolver, checkField);
-
 
     if (checkField === false) {
         const result = await resolve(root, args, context, info)
@@ -43,39 +41,50 @@ const checkToken = async (resolve, root, args, context, info) => {
         return result
     } else if (checkField === true) {
         console.log('================= IN MIDDLEWARE 1 - Controllo del token');
+
         // Token da FE
-        let token = context[0].authorization;
+        let token: string = context[0].authorization;
         // Dati Utente da FE
-        let userData = JSON.parse(context[0].userdata);
-        // console.log(token, userData);
+        let userData: any = context[0].userdata;
 
-        // Decrypt Token
-        let decoded = await jwt.verify(token, secret.secret);
+        console.log('DATATI QL: ', token, userData);
 
-        const user = await prisma.utenti.findUnique({
-            where: {
-                id: +decoded.id
+        if (token && userData) {
+
+            userData = JSON.parse(context[0].userdata)
+
+            // Decrypt Token
+            let decoded = await jwt.verify(token, secret.secret);
+
+            const user = await prisma.utenti.findUnique({
+                where: {
+                    id: +decoded.id
+                }
+            })
+
+            const gruppo = await prisma.gruppo.findUnique({
+                where: {
+                    id: +user.idGruppo
+                }
+            })
+
+            // Controllo se l'utente appartiene ai gruppi che possono fare la chiamata
+            let isInGroup = rolesRoute.includes(gruppo.descrizione);
+            let compareIds = +decoded.id === +userData.id ? true : false;
+
+            if (compareIds && isInGroup) {
+                const result = await resolve(root, args, context, info)
+                console.log('================= IN MIDDLEWARE 2 - Utente può fare la chiamata');
+                return result
+            } else {
+                console.log('================= IN MIDDLEWARE 2 - Utente non autorizzato fare la chiamata');
+                throw new Error('Non sei autorizzato ad effettuare questa operazione');
             }
-        })
-
-        const gruppo = await prisma.gruppo.findUnique({
-            where: {
-                id: +user.idGruppo
-            }
-        })
-
-        // Controllo se l'utente appartiene ai gruppi che possono fare la chiamata
-        let isInGroup = rolesRoute.includes(gruppo.descrizione);
-        let compareIds = +decoded.id === +userData.id ? true : false;
-
-        if (compareIds && isInGroup) {
-            const result = await resolve(root, args, context, info)
-            console.log('================= IN MIDDLEWARE 2 - Utente può fare la chiamata');
-            return result
         } else {
             console.log('================= IN MIDDLEWARE 2 - Utente non autorizzato fare la chiamata');
-            throw new Error('Non sei autorizzato ad effettuazre questa operazione');
+            throw new Error('Errore - Dati non pervenuti');
         }
+
     }
 
 }
