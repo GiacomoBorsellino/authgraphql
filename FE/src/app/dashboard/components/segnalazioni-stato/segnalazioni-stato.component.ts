3a01b1ce56bacc23afc09bd6074e483c1f6bfef3
Giacomo <giacomo.borsellino@avrgroup.it>
2023-03-14 15:55:36 +0100

    Updates

--- FE/src/app/dashboard/components/segnalazioni-stato/segnalazioni-stato.component.ts
+++ FE/src/app/dashboard/components/segnalazioni-stato/segnalazioni-stato.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { Chart, ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
+import { Component } from '@angular/core';
+import { ChartConfiguration, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-segnalazioni-stato',
@@ -8,36 +7,79 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrls: ['./segnalazioni-stato.component.css'],
 })
 export class SegnalazioniStatoComponent {
-  private newLabel? = 'New label';
-
   constructor() {}
+
+  public lineChartType: ChartType = 'line';
+
+  public startDate: string = new Date().getFullYear() + '-01-01T00:00:00';
+  public endDate: string = new Date().getFullYear() + '-12-31T00:00:00';
+
+  public yearSelected: boolean = true;
+  public monthSelected: boolean = false;
+
+  public months: string[] = [
+    'Gennaio',
+    'Febbraio',
+    'Marzo',
+    'Aprile',
+    'Maggio',
+    'Giugno',
+    'Luglio',
+    'Agosto',
+    'Settembre',
+    'Ottobre',
+    'Novembre',
+    'Dicembre',
+  ];
+
+  selectRange(value: any) {
+    console.log('Valore: ', value);
+    this.monthSelected = !this.monthSelected;
+  }
+
+  year() {}
+
+  month() {}
 
   public lineChartData: ChartConfiguration['data'] = {
     datasets: [
       {
-        data: [65, 59, 80, 81, 56, 55, 40],
+        data: [65, 59, 80, 81, 56, 55, 40, 28, 48, 40, 19, 86, 27, 90],
         label: 'Series A',
         backgroundColor: 'rgba(148,159,177,0.2)',
         borderColor: 'rgba(148,159,177,1)',
         pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: 'white',
-        pointHoverBackgroundColor: 'grey',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgba(148,159,177,0.8)',
         fill: 'origin',
       },
       {
-        data: [28, 48, 40, 19, 86, 27, 90],
+        data: [28, 48, 40, 19, 86, 27, 90, 28, 48, 40, 19, 86, 27, 90],
         label: 'Series B',
         backgroundColor: 'rgba(77,83,96,0.2)',
         borderColor: 'rgba(77,83,96,1)',
         pointBackgroundColor: 'rgba(77,83,96,1)',
-        pointBorderColor: 'white',
-        pointHoverBackgroundColor: 'grey',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgba(77,83,96,1)',
         fill: 'origin',
       },
     ],
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: [
+      'Gennaio',
+      'Febbraio',
+      'Marzo',
+      'Aprile',
+      'Maggio',
+      'Giugno',
+      'Luglio',
+      'Agosto',
+      'Settembre',
+      'Ottobre',
+      'Novembre',
+      'Dicembre',
+    ],
   };
 
   public lineChartOptions: ChartConfiguration['options'] = {
@@ -50,21 +92,6 @@ export class SegnalazioniStatoComponent {
       // We use this empty structure as a placeholder for dynamic theming.
       y: {
         position: 'left',
-        grid: {
-          color: 'white',
-        },
-        ticks: {
-          color: 'white',
-        },
-      },
-      y1: {
-        position: 'right',
-        grid: {
-          color: 'white',
-        },
-        ticks: {
-          color: 'white',
-        },
       },
     },
 
@@ -72,29 +99,4 @@ export class SegnalazioniStatoComponent {
       legend: { display: true },
     },
   };
-
-  public lineChartType: ChartType = 'line';
-
-  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
-
-  // events
-  public chartClicked({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: {}[];
-  }): void {
-    // console.log(event, active);
-  }
-
-  public chartHovered({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: {}[];
-  }): void {
-    // console.log(event, active);
-  }
 }
